9bd9dced84ef1bab4ea4ed396f9c61ff32386e58
Dat Ngo <ngodat02092001@gmail.com>
2023-02-06 10:38:12 +0700

    "khoi dong server"

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
-
+var mongoose = require('mongoose');
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
+const dbUrl = "mongodb://localhost:27017/CVHT_database";
+
+mongoose
+.connect(dbUrl, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+    },
+    (err) => {
+        if (err) console.log('error');
+        else console.log('db connected');
+    })
+    
 app.listen(PORT, (err) => {
     if (err) console.log('error');
     else console.log(`server listening on ${PORT}`);
